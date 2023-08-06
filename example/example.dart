c6980e5f2caf27d4373f83923f880b7f939161cb
Duy Nguyen Ngoc <duynn@nal.vn>
2023-08-06 13:56:38 +0700

    [DMT-01] Update example, test, README

--- example/example.dart
+++ example/example.dart
@@ -1,30 +1,38 @@
 import 'package:dmutex/dmutex.dart';
 
 void main() async {
-  // Example usage of the DLock class
-  final dLock = DLock();
+  final dLock = DLock(); // Create a DLock object
 
+  // Example 1: Use DLock to ensure only one task executes at a time
   await dLock.withLock(() async {
-    print("Acquired DLock");
+    print('Job 1 is executing...');
     await Future.delayed(Duration(seconds: 2));
-    print("Released DLock");
+    print('Job 1 has completed.');
   });
 
-  // Example usage of the ReentrantDLock class
+  // Example 2: Use ReentrantDLock to control concurrency across multiple levels
   final reentrantDLock = ReentrantDLock();
-
   await reentrantDLock.withLock(() async {
-    print("Acquired ReentrantDLock");
-
+    print('Outer-level Job 3 is executing...');
     await reentrantDLock.withLock(() async {
-      print("Acquired Nested ReentrantDLock");
+      print('Inner-level Job 3 is executing...');
       await Future.delayed(Duration(seconds: 1));
-      print("Released Nested ReentrantDLock");
+      print('Inner-level Job 3 has completed.');
     });
-
-    await Future.delayed(Duration(seconds: 2));
-    print("Released ReentrantDLock");
+    print('Outer-level Job 3 has completed.');
   });
 
-  print("Example completed");
+  // Example 3: Use DLock to control synchronization of API calls
+  await Future.wait([
+    dLock.withLock(() async {
+      print('Calling API 1...');
+      await Future.delayed(Duration(seconds: 2));
+      print('API 1 call has completed.');
+    }),
+    dLock.withLock(() async {
+      print('Calling API 2...');
+      await Future.delayed(Duration(seconds: 1));
+      print('API 2 call has completed.');
+    }),
+  ]);
 }
--- test/dlock_test.dart
+++ test/dlock_test.dart
@@ -2,7 +2,7 @@ import 'package:test/test.dart';
 import 'package:dmutex/dmutex.dart';
 
 void main() {
-  group('DLock', () {
+  group('DLock Tests', () {
     test('Lock and unlock', () async {
       final dLock = DLock();
 
@@ -13,6 +13,25 @@ void main() {
       expect(dLock.isLocked, isFalse);
     });
 
-    // Add more test cases as needed
+    test('Wait for lock release', () async {
+      final dLock = DLock();
+
+      Future<void> lockedTask() async {
+        await dLock.lock();
+        await Future.delayed(Duration(seconds: 1));
+        dLock.unlock();
+      }
+
+      Future<void> waitingTask() async {
+        await dLock.wait();
+      }
+
+      final locked = lockedTask();
+      await Future.delayed(Duration(milliseconds: 500));
+      final waiting = waitingTask();
+
+      final results = await Future.wait([locked, waiting]);
+      expect(results, [null, null]);
+    });
   });
 }
